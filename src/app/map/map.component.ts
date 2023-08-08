863c363ecaf43e331b19b147138295b263446462
elevchyt <lehy64@gmail.com>
2023-08-08 13:14:53 +0300

    get user geolocation & set position and zoom levels

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,10 @@
 import { Component, AfterViewInit } from '@angular/core';
 
 declare var H: any;
+type GeolocationType = {
+  lat: number;
+  lng: number;
+};
 
 @Component({
   selector: 'app-map',
@@ -8,7 +12,8 @@ declare var H: any;
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements AfterViewInit {
-  platform;
+  platform: any;
+  map: any;
 
   constructor() {
     this.platform = new H.service.Platform({
@@ -17,26 +22,44 @@ export class MapComponent implements AfterViewInit {
     });
   }
 
-  setCenterToLocation(lat: number, lng: number, zoomLevel: number) {
-    
-  }
-
   initMap(): void {
     var defaultLayers = this.platform.createDefaultLayers();
 
-    var map = new H.Map(document.getElementById('map-container'),
-      defaultLayers.vector.normal.map,{
-      center: {lat:50, lng:5},
-      zoom: 4,
-      pixelRatio: window.devicePixelRatio || 1
+    this.map = new H.Map(
+      document.getElementById('map-container'),
+      defaultLayers.vector.normal.map,
+      {
+        center: { lat: 50, lng: 5 },
+        zoom: 4,
+        pixelRatio: window.devicePixelRatio || 1,
+      }
+    );
+    window.addEventListener('resize', () => this.map.getViewPort().resize());
+
+    var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    var ui = H.ui.UI.createDefault(this.map, defaultLayers);
+  }
+
+  setCenterToLocation(lat: number, lng: number, zoomLevel: number) {
+    this.map.setCenter({lat, lng});
+    this.map.setZoom(zoomLevel, true)
+  }
+
+  getUserLocation(): Promise<GeolocationPosition> {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 10000,
+    };
+
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options)
     });
-    window.addEventListener('resize', () => map.getViewPort().resize());
-    
-    var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-    var ui = H.ui.UI.createDefault(map, defaultLayers);
   }
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.getUserLocation().then(position => {
+      this.setCenterToLocation(position.coords.latitude, position.coords.longitude, 15);
+    });
   }
 }
