b0fe0ce0d0bdbdfae26abdcc67d696fd9a243f1d
elevchyt <lehy64@gmail.com>
2023-08-11 03:52:55 +0300

    data flow in order to populate route modal with fetched data

--- src/app/components/route-modal/route-modal.component.ts
+++ src/app/components/route-modal/route-modal.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from "@angular/core";
-import { BusService } from "src/app/services/bus.service";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BusService } from 'src/app/services/bus.service';
 
 @Component({
-  selector: "app-route-modal",
-  templateUrl: "./route-modal.component.html",
-  styleUrls: ["./route-modal.component.scss"]
+  selector: 'app-route-modal',
+  templateUrl: './route-modal.component.html',
+  styleUrls: ['./route-modal.component.scss'],
 })
-
-export class RouteModalComponent implements OnInit {
-  routes: Array<object> = [];
+export class RouteModalComponent implements OnInit, OnDestroy {
+  subscriptions: Array<Subscription> = [];
   isModalOpen: boolean = false;
+  routes: Array<any> = [];
 
-  constructor(public busService: BusService) { 
-
+  constructor(public busService: BusService) {
+    // Open modal with fetched routes
+    const subscription: Subscription = this.busService
+      .openBusRoutesModalListen()
+      .subscribe((routes: any) => {
+        this.routes = routes;
+        this.setModalOpen(true);
+      });
+    this.subscriptions.push(subscription);
   }
 
-  ngOnInit() {
+  setModalOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
 
+  onRouteSelect(routeCode: string) {
+    this.setModalOpen(false);
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
