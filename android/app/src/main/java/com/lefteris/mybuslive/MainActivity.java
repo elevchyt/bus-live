6cc9ed7f18f1ac5096ff5d4943a5fd952219be44
elevchyt <lehy64@gmail.com>
2024-09-21 18:22:54 +0300

    capacitor + android build

---
+++ android/app/src/main/java/com/lefteris/mybuslive/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lefteris.mybuslive;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lefteris.mybuslive',
+  appName: 'myBus Live',
+  webDir: 'dist'
+};
+
+export default config;
